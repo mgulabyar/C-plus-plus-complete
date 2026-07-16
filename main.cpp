868fecb28336aa8ffc2f2778e25e728d6316c77d
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2026-07-16 23:37:09 +0500

    feat: implemented system runtime information logs

--- main.cpp
+++ main.cpp
@@ -4,5 +4,8 @@ int main() {
     std::cout << "========================================\n";
     std::cout << "   COMPILER AND ARCHITECTURE SUCCESS   \n";
     std::cout << "========================================\n";
+        std::cout << "Compiler Status: GCC 16.1.0 Active\n";
+    std::cout << "Environment   : Win-64 Bit Stable\n";
+
     return 0;
 }
