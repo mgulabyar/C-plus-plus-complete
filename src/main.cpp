e60ee80de44ebbc21104ad3e51448f7eee2c2a78
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2026-07-18 01:15:30 +0500

    Fix error inside

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,7 @@
-#include "crow_all.h"                     // Direct reference target
-#include "include/routes/ProductRoutes.h" // Root configuration tracking
 #include <iostream>
+using namespace std;
 
 int main() {
-    crow::SimpleApp app;
-    std::cout << "[SERVER] Initializing stable MongoDB local system pipeline driver...\n";
-    ProductRoutes::init(app);
-    app.port(18080).multithreaded().run();
-    return 0;
+  cout << "Hello World!";
+  return 0;
 }
